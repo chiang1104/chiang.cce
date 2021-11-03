c9fb42b90793b0ca67f44c7e397e61a38ed262bb
蔣名曜 <91170675+chiang1104@users.noreply.github.com>
2021-11-03 11:55:19 +0800

    Add files via upload

---
+++ week8/week08-1.cpp
@@ -0,0 +1,14 @@
+#include <Stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+
+	int sum=0,now=1;
+	for(int i=1;i<=n;i++)
+	{
+		now *= i;
+		sum += now;
+	}
+	printf("請輸入任一整數:1!到5!的總和為=%d \n",sum);
+}
